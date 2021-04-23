a5e01f5097e0d53214947f29aae480c6a6d689e9
Alain Kubwayo <alainkubwayo@Alains-MacBook-Pro.local>
2021-04-23 15:50:45 +0200

    modify welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
 
     <h1>Project Ideas</h1>
     <div class="all-projects">
-            @foreach($projects as $project)
-                <div class="project-content">
-                    <h2>{{$project->name}}</h2>
-                    <p>{{$project->desc}}</p>
-                    <a href="/projects/{{$project->slug}}">View Full Details</a>
-                </div>
-            @endforeach
+        @foreach($projects as $project)
+            <div class="project-content">
+                <h2>{{$project->name}}</h2>
+                <p>{{$project->desc}}</p>
+                <a href="/projects/{{$project->slug}}">View Full Details</a>
+            </div>
+        @endforeach
     </div>
 
 @endsection
